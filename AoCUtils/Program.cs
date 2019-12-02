bfda8fe966fee9417ee975d7d94e838e92f18efa
Matt Morgan <mmorgan@miradorfin.com>
2019-12-01 19:14:15 -0800

    Init and setup

---
+++ AoCUtils/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Utils
+{
+    public static class Files
+    {
+        public static List<int> ReadLines()
+        {
+            return new List<int>(){0, 2};
+        }
+        
+        static void Main(string[] args) { }
+    }
+}
---
+++ Day01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualBasic.CompilerServices;
+
+using NUnit.Framework;
+using Utils;
+
+namespace Day01
+{
+    class Program
+    {
+        static int Part01()
+        {
+            return 0;
+        }
+
+        static int Part02()
+        {
+            var data = Files.ReadLines();
+            return data[0];
+        }
+        
+        [Test]
+        public void Part01Test()
+        {
+            Assert.AreEqual(0, Part01());
+        }
+
+        [Test]
+        public void Part02Test()
+        {
+            Assert.AreEqual(0, Part02());
+        }
+        
+    }
+}
