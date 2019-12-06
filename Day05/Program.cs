b7c0a6d647d0d205f2073bba64aff4b5a3865cb0
Matt Morgan <mmorgan@miradorfin.com>
2019-12-05 23:22:49 -0800

    Day05: part2 and cleanup

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -8,7 +8,7 @@ namespace Day05
 {
     internal class Program
     {
-        private static int Part01(int[] input)
+        private static int Part01(int[] input, int initialInput)
         {
             var output = 0;
             var pc = 0;
@@ -37,13 +37,52 @@ namespace Day05
                             pc += 4;
                             break;
                         case 3:
-                            input[input[pc + 1]] = 1;
+                            input[input[pc + 1]] = initialInput;
                             pc += 2;
                             break;
                         case 4:
-                            Console.WriteLine($"Output: {input[input[pc + 1]]}");
                             output = input[input[pc + 1]];
                             pc += 2;
+                            break;
+                        case 5:
+                            first = Convert.ToBoolean(opCode.Item2[0]) ? pc + 1 : input[pc + 1];
+                            second = Convert.ToBoolean(opCode.Item2[1]) ? pc + 2 : input[pc + 2];
+                            if (input[first] > 0)
+                            {
+                                pc = input[second];
+                            }
+                            else
+                            {
+                                pc += 3;
+                            }
+
+                            break;
+                        case 6:
+                            first = Convert.ToBoolean(opCode.Item2[0]) ? pc + 1 : input[pc + 1];
+                            second = Convert.ToBoolean(opCode.Item2[1]) ? pc + 2 : input[pc + 2];
+                            if (input[first] == 0)
+                            {
+                                pc = input[second];
+                            }
+                            else
+                            {
+                                pc += 3;
+                            }
+
+                            break;
+                        case 7:
+                            first = Convert.ToBoolean(opCode.Item2[0]) ? pc + 1 : input[pc + 1];
+                            second = Convert.ToBoolean(opCode.Item2[1]) ? pc + 2 : input[pc + 2];
+                            third = input[pc + 3];
+                            input[third] = input[first] < input[second] ? 1 : 0;
+                            pc += 4;
+                            break;
+                        case 8:
+                            first = Convert.ToBoolean(opCode.Item2[0]) ? pc + 1 : input[pc + 1];
+                            second = Convert.ToBoolean(opCode.Item2[1]) ? pc + 2 : input[pc + 2];
+                            third = input[pc + 3];
+                            input[third] = input[first] == input[second] ? 1 : 0;
+                            pc += 4;
                             break;
                         default:
                             Console.WriteLine($"Found unknown OP Code {opCode.Item1}");
@@ -52,12 +91,10 @@ namespace Day05
                 }
                 catch (System.IndexOutOfRangeException)
                 {
-                    Console.WriteLine($"some index error {opCode.Item1} {opCode.Item2.Count}");
-                    return 0;
+                    return -1;
                 }
-
-                
             }
+
             return output;
         }
 
@@ -78,47 +115,15 @@ namespace Day05
             var expected = Tuple.Create(2, new List<int>() {0, 1, 0});
             Assert.AreEqual(expected, ProcessOpCode(1002));
         }
-        
-        private static int Part02(int[] input, int result)
-        {
-            for (var i = 0; i < 100; i++)
-            {
-                for (var j = 0; j < 100; j++)
-                {
-                    int[] freshInput = new int[input.Length];
-                    input.CopyTo(freshInput, 0);
-                    freshInput[1] = i;
-                    freshInput[2] = j;
-                    var returnedFromPart01 = Part01(freshInput);
-                
-                    if (returnedFromPart01 == result) return (100 * i) +j;
-                }
-            }
 
-            return 0;
-        }
-        
-        
+
         [Test]
-        public void Part01Test()
+        [TestCase(13787043, 1)] // TestCase Part 1
+        [TestCase(3892695, 5)]  // TestCase Part 2
+        public void Part01Test(int expected, int initial)
         {
             var input = Files.ReadFileAsArrayOfInt("/home/mmorgan/src/adventofcode2019/Day05/input");
-            Assert.AreEqual(13787043, Part01(input));
-        }
-
-        [Test]
-        public void Part02ExampleTest()
-        {
-            // No real example test
-            Assert.True(true);
-        }
-        
-        [Test]
-        public void Part02Test()
-        {
-            var result = 19690720;
-            var input = Files.ReadFileAsArrayOfInt("/home/mmorgan/src/adventofcode2019/Day02/input");
-            Assert.AreEqual(5064, Part02(input, result));
+            Assert.AreEqual(expected, Part01(input, initial));
         }
     }
 }
