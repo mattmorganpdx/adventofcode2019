71987c6c3aa07fa7d76e84c31b9d5a82d7aa7bcb
Matt Morgan <mmorgan@miradorfin.com>
2019-12-06 23:26:57 -0800

    Day07: setup for part 2

--- Day07/Program.cs
+++ Day07/Program.cs
@@ -28,7 +28,7 @@ namespace Day07
             return maxScore;
         }
 
-        private static int Part02(List<string> input)
+        private static int Part02(int[] input)
         {
             return 0;
 
@@ -74,7 +74,21 @@ namespace Day07
         [Test]
         public void Part02ExampleTest()
         {
-            Assert.AreEqual(0, 0);
+            var input = new List<int>()
+            {
+                3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26,
+                27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5
+            }.ToArray();
+            Assert.AreEqual(139629729, Part02(input));
+
+            input = new List<int>()
+            {
+                3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001, 54,
+                -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4,
+                53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10
+            }.ToArray();
+            Assert.AreEqual(18216, Part02(input));
+
         }
 
         [Test]
