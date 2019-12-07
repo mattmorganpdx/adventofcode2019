aabe00204cc71e461d2cc322589a192a8c0eb487
Matt Morgan <mmorgan@miradorfin.com>
2019-12-06 20:51:27 -0800

    Day07: setup project

---
+++ Day07/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using NUnit.Framework;
+using Utils;
+
+namespace Day07
+{
+    internal class Program
+    {
+        private static int Part01(IEnumerable<string> input)
+        {
+            return 0;
+        }
+
+        private static int Part02(List<string> input)
+        {
+            return 0;
+
+        }
+
+        [Test]
+        public void Part01ExampleTest()
+        {
+            Assert.AreEqual(0, 0);
+        }
+
+        [Test]
+        public void Part01Test()
+        {
+            var input = System.IO.File.ReadLines("/home/mmorgan/src/adventofcode2019/Day06/input");
+            Assert.AreEqual(0, 0);
+        }
+
+        [Test]
+        public void Part02ExampleTest()
+        {
+            Assert.AreEqual(0, 0);
+        }
+
+        [Test]
+        public void Part02Test()
+        {
+            var input = System.IO.File.ReadLines("/home/mmorgan/src/adventofcode2019/Day06/input").ToList();
+            Assert.AreEqual(0, 0);
+        }
+    }
+}
